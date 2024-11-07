7cd0affd499c23986882e3941e3b9d4207c64279
kubo <mohammed.swidan@naseej.com>
2024-11-07 13:43:45 +0000

    remove old

--- upgrade-scripts/declare-all-modules.sh
+++ upgrade-scripts/declare-all-modules.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+curl -w '\n' -D - -X POST -H "Content-type: application/json" \
+   -d '{ "urls": [ "https://folio-registry.dev.folio.org" ] }' \
+  "$dest_url/_/proxy/pull/modules"
+
 for i in `cat frontend-modules`; do echo $i; curl --location --request POST "$dest_url/_/proxy/modules" --header "x-okapi-token: $okapitoken" --header 'Cookie: PORTALSESSION=okapi-stg' --header 'Content-Type: application/json' --data-raw "`curl -s --location --request GET "$source_url/_/proxy/modules/${i}" --header "x-okapi-token: $okapitoken" --header 'Cookie: PORTALSESSION=okapi-stg'`"; done
 
 
--- upgrade-scripts/env.sh
+++ upgrade-scripts/env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
 export source_url="https://okapi.medadstg.com" 
-export dest_url="https://okapi.medadstg.com" 
+export dest_url="https://okapi.dev.mynaseej.net" 
 export okapitoken="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdXBlcnVzZXIiLCJ0eXBlIjoibGVnYWN5LWFjY2VzcyIsInVzZXJfaWQiOiI5OTk5OTk5OS05OTk5LTQ5OTktOTk5OS05OTk5OTk5OTk5OTkiLCJpYXQiOjE2NjcyMjQ2MzUsInRlbmFudCI6InN1cGVydGVuYW50In0.gJAnqObQYE_fnVVkhQyVF-98eqHBmEGZ0lPITKtjc-Q"
